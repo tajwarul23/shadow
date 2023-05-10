0b7fe53a90bca1983305aada07532a31e69548ae
tajwarul23 <tajwarulchowdhury@gmail.com>
2023-05-10 15:52:46 +0600

    navbar_done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 // // import PhoneIcon from '@mui/icons-material/Phone';
 // import PhoneIcon from '@mui/icons-material/Phone';
 // import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 
 
 import photo from './shadow.jpg';
+import About from './components/About';
+import Service from './components/Service';
+import Contact from './components/Contact';
 function App() {
   return (
-    <div className="App bg-neutral-700">
+    <div className="App bg-neutral-700 min-h-screen">
    <Navbar></Navbar>
-
-   {/* <Home></Home> */}
+    <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/home' element={<Home></Home>}></Route>
+      <Route path='/about' element={<About></About>}></Route>
+      <Route path='/service' element={<Service></Service>}></Route>
+      <Route path='/contact' element={<Contact></Contact>}></Route>
+    </Routes>
+   
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="bg-gray-100 w-full md:w-4/5 lg:w-3/5 mx-auto">
+      <div className="flex flex-col md:flex-row">
+        <div className="bg-white p-8 md:w-3/5">
+          {/* Content for the larger column */}
+          <h2 className="text-2xl font-bold mb-4">Large Column</h2>
+          <p className="text-gray-600">This is the larger column that covers 70% of the width.</p>
+          {/* Add additional padding/margin to increase height */}
+          <div className="mt-8">
+            {/* Additional content */}
+          </div>
+        </div>
+        <div className="bg-orange-500 p-8 md:w-2/5">
+          {/* Content for the smaller column */}
+          <h2 className="text-2xl font-bold mb-4 text-white">Small Column</h2>
+          <p className="text-white">This is the smaller column that covers 30% of the width and has an orange background.</p>
+          <div className='mt-2'>
+          <Link className="btn btn-active " to="tel:+143-723-39078">Call Now</Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
